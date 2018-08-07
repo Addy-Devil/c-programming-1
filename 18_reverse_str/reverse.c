aa4abaa50376c311677218f56f80fe1d4beeb509
Coursera Learner <nobody@nowhere.nul>
2018-08-07 21:23:51 +0000

    stuff

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,7 +3,27 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  //find the length of the string
+  int len = 0;
+  //printf("Before ++, str: %d\n", *str);
+  while (*str != 0) {
+    len++;
+    str++;
+  }
+  //printf("Before reset, str: %d\n", *str);
+  // reset the pointer to the beginning of the string
+  str -= len;
+  //printf("After reset, str: %d\n", *str);
+
+  // loop over half the string
+  // swapping the ith value with the (len-i)th value
+  for (int i=0; i<len/2; i++) {
+    char tmp = str[i];
+    //printf("i: %d and tmp: %c\n", i, tmp);
+    //printf("str[len-i] = %c\n", str[len-i-1]);
+    str[i] = str[len-i-1];
+    str[len-i-1] = tmp;
+  }
 }
 
 int main(void) {
