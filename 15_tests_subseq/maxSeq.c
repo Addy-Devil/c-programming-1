e828ca5c3745671eaa92e2df00d51610722c9d13
Coursera Learner <nobody@nowhere.nul>
2018-08-07 05:23:33 +0000

    stuff

--- 15_tests_subseq/maxSeq.c
+++ 15_tests_subseq/maxSeq.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t maxSeq(int * array, size_t n) {
+size_t maxSeq(int * array, int n) {
   // if there are no elements, return NULL
-  if (n==0) {
-    return NULL;
+  if (n<1 || array==NULL) {
+    return 0;
   }
   // keep track of max sequence length
   size_t maxLength = 1;
   // keep track of current sequence length
   size_t seqLength = 1;
   // iterate over all the elements in an array
-  size_t i = 1;
+  int i = 1;
   while (i < n) {
     // if the ith element is greater than the (i-1)th
     // element, increment current sequence length
     if (array[i] > array[i-1]) {
+      printf("array[%d] > array[%d]\n", i, i-1);
       seqLength++;
+      printf("seqLength = %d\n", (int)seqLength);
+      // if we have checked all the items in the array
+      // and last element of the array is part of the
+      // largest strictly increasing sequence,
+      // set the length of the max sequence
+      if (seqLength > maxLength) {
+        maxLength = seqLength;
+	printf("i = %d, maxLength = %d\n", i, (int)maxLength);
+      }
     }
     // otherwise, check if the length of the currrent
     // sequence is greater than the length of the
     // longest sequence
     else {
+      printf("array[%d] <= array[%d]\n", i, i-1);
+      /*
       if (seqLength > maxLength) {
 	// if the current sequence is longer,
 	// update the max sequence length
 	maxLength = seqLength;
+	printf("maxLength updated to: %d\n", maxLength);
       }
+      */
       // reset the length of the current sequence
       seqLength = 1;
+      printf("reset seqLength\n");
     }
     // increment the array index to check
     i++;
+    printf("i incremented to: %d\n", i);
   }
   // return the length of the longest sequence
   return maxLength;
--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t maxSeq(int * array, size_t n);
+size_t maxSeq(int * array, int n);
 
 void doTest(int * array, int n) {
+  int p = maxSeq(array, n);
   printf("maxSeq(");
-  if (array==NULL) {
+  if (array==NULL || n<1) {
     printf("NULL");
   }
   else {
@@ -19,8 +20,7 @@ void doTest(int * array, int n) {
     printf("}");
   }
   printf(", %d) is \n", n);
-  int p = maxSeq(array, n);
-  if (p == NULL) {
+  if (p == 0) {
     printf("NULL\n");
   }
   else {
@@ -34,14 +34,22 @@ int main(void) {
   int array2[] = {3, 1, 3, 5, 7, 2, 3}; // n=7
   int array3[] = {5000, 62, -7, 100, 0};// n=5
   int array4[] = {3, 2, 1, 1}; // n=4
-
+  int array5[] = {-1, -2, -1, -12}; // n=4
+  int array6[] = {1, 2, 1, 2, 3, 4}; //n=6
+  int array7[] = {1, 2, 1, 2}; // n=4
+  
   doTest(array1, 6);
   doTest(array2, 7);
   doTest(array3, 5);
   doTest(array4, 4);
+  doTest(array5, 4);
+  doTest(array6, 6);
+  doTest(array7, 4);
   doTest(NULL, 0);
   doTest(array1, 0);
+  doTest(array1, 1);
   doTest(array1, -1);
+  doTest(array1, 60);
   
   return EXIT_SUCCESS;
 }
