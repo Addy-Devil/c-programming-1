b78462cb312b193251f97809c7833ee6ab7cdf06
Coursera Learner <nobody@nowhere.nul>
2018-08-27 20:57:11 +0000

    stuff

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,66 +16,12 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-size_t getNumLines(FILE * f) {
-  size_t n = 0;
-  char * line = NULL;
-  size_t sz = 0;
-  while(getline(&line, &sz, f) > 0) {
-    printf("In getNumLines: %s\n", line);
-    n++;
-  }
-  free(line);
-  f -= n;
-  return n;
-}
-
 int main(int argc, char ** argv) {
-  /*
-  if (argc==1) {
-    //read from stdin
-    FILE * f = stdin;
-  }
-  else {
-    //read from argvs
-    FILE * f = fopen(argv[1], "r");
-  }
-  */
-
-  // store lines of input so they can be sorted
-  
-  
   
   //WRITE YOUR CODE HERE!
   if(argc==1) {
-    char * line = NULL;
-    size_t sz = 0;
-
-    extern FILE * stdin;
-    if (stdin==NULL) {
-      fprintf(stderr, "You must enter at least a line of text.\n");
-      return EXIT_FAILURE;
-    }
-    // get the number of lines to read and malloc an array to hold them
-    size_t numLines = getNumLines(stdin);
-    char ** lines = malloc(numLines * sizeof(*lines));
-    // write each line into the lines array
-    size_t i = 0;
-    while(getline(&line, &sz, stdin) > 0) {
-      lines[i] = line;
-      i++;
-    }
-    // free last line from getline
-    free(line);
-    // sort the data
-    sortData(lines, numLines);
-
-    // print the lines that have been sorted
-    for (i=0; i<numLines; i++) {
-      printf("%s\n", lines[i]);
-    }
-    
-    // free malloced lines array
-    free(lines);
+    printf("Work on tmp.txt first\n");
+    return EXIT_FAILURE;
   }
   else {
     size_t i = 1;
@@ -89,26 +35,45 @@ int main(int argc, char ** argv) {
       }
 
       // get the number of lines to read and malloc an array to hold them
+      /*
       size_t numLines = getNumLines(f);
-      if (fclose(f) != 0) {
-	perror("File did not close.");
-	return EXIT_FAILURE;
-      }
       printf("num lines in file: %s : %zu\n", argv[i],  numLines);
-      char ** lines = malloc(numLines * sizeof(char*));
+      */
       // write each line into the lines array
       size_t j = 0;
-      size_t lineLen = 0;
-      f = fopen(argv[i], "r");
+      ssize_t lineLen = 0;
+      size_t numLines = 1;
+      char ** lines = malloc(numLines * sizeof(char*));
       while((lineLen=getline(&line, &sz, f)) > 0) {
 	lines[j] = malloc((lineLen+1) * sizeof(char));
 	lines[j] = line;
 	j++;
+	numLines++;
+	lines = realloc(lines, numLines * sizeof(char*));
       }
+
+      // if the file has no lines of content, then j will not have incremented
+      // return an error
+      if (j == 0) {
+	fprintf(stderr, "File (%s) must contain at least one line of text.\n", argv[i]);
+	return EXIT_FAILURE;
+      }
+      
+      // while loop right above adds 1 too many to numLines and reallocs to `lines`,
+      // so take them away
+      if (numLines > 1) {
+	numLines--;
+	lines = realloc(lines, numLines * sizeof(char*));
+      }
+      
+      // DELETE THIS LATER
+      // check array has correct info in it...
       printf("Pre sorted array.\n");
       for (int k=0;k<numLines;k++) {
-	printf("%s\n", lines[k]);
+	printf("%s", lines[k]);
       }
+      // DELETE THIS LATER
+      
       // free last line from getline
       free(line);
       // sort the data
