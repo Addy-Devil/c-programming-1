d99714b2f0737e4681ec1eee912eff94232a6090
Coursera Learner <nobody@nowhere.nul>
2018-08-27 22:17:34 +0000

    stuff

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -20,8 +20,52 @@ int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
   if(argc==1) {
-    printf("Work on tmp.txt first\n");
-    return EXIT_FAILURE;
+    FILE * f = stdin;
+    if (f==NULL) {
+      fprintf(stderr, "Failed to open file from stdin\n");
+      return EXIT_FAILURE;
+    }
+    // write each line into the lines array
+    size_t j = 0;
+    //ssize_t lineLen = 0;
+    //size_t numLines = 0;
+    char ** lines = NULL;
+    char * line = NULL;
+    size_t sz = 0;
+    while(getline(&line, &sz, f) >= 0) {
+      lines = realloc(lines, (j+1) * sizeof(*lines));
+      //lines[j] = malloc((lineLen+1) * sizeof(char));
+      lines[j] = line;
+      line = NULL;
+      j++;
+    }
+    free(line);
+    // if the file has no lines of content, then j will not have incremented
+    // return an error
+    if (j == 0) {
+      fprintf(stderr, "Input must contain at least one line of text.\n");
+      return EXIT_FAILURE;
+    }
+      
+    // sort the data
+    sortData(lines, j);
+
+    // print the lines that have been sorted
+    int k;
+    for (k=0; k<j; k++) {
+      printf("%s\n", lines[k]);
+    }
+
+    // free each line of lines
+    for (k=0; k<j; k++) {
+      free(lines[k]);
+    }
+    // free malloced lines array
+    free(lines);
+    if (fclose(f) != 0) {
+      perror("Failed to close the file!");
+      return EXIT_FAILURE;
+    }
   }
   else {
     size_t i = 1;
@@ -43,8 +87,6 @@ int main(int argc, char ** argv) {
 	//lines[j] = malloc((lineLen+1) * sizeof(char));
 	lines[j] = line;
 	line = NULL;
-	printf("for lines[%zu]: sz = %zu\n", j, sz);
-	printf("%s\n", lines[j]);
 	j++;
       }
       free(line);
