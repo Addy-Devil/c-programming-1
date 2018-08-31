ef9a7eedd7b2e0a65456c31709b3738e198b0b32
Coursera Learner <nobody@nowhere.nul>
2018-08-31 21:24:25 +0000

    stuff

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -37,7 +37,7 @@ void assert_full_deck(deck_t * d) {
 
 void add_card_to(deck_t * deck, card_t c) {
   deck->n_cards++;
-  deck->cards = realloc(deck->cards, deck->n_cards * sizeof(*deck->cards));
+  deck->cards = realloc(deck->cards, deck->n_cards * sizeof(deck->cards));
   deck->cards[deck->n_cards-1] = &c;
 }
 
@@ -77,7 +77,6 @@ deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) {
   
   return d2;
 }
-
 
 void free_deck(deck_t * deck) {
   for (size_t i=0; i<deck->n_cards; i++) {
---
+++ c4prj1_deck/test-c4deck.c
@@ -0,0 +1,14 @@
+#include <sdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cards.h"
+#include "deck.h"
+#include "eval.h"
+
+
+
+int main(int argc, char ** argv) {
+  bla;
+
+  exit(EXIT_SUCCESS);
+}
