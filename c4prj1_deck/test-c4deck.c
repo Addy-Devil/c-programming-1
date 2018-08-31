6b77ba760ae098a9dfaac259b2faca3aa787c1f2
Coursera Learner <nobody@nowhere.nul>
2018-08-31 23:32:20 +0000

    stuff

--- c4prj1_deck/test-c4deck.c
+++ c4prj1_deck/test-c4deck.c
@@ -1,4 +1,4 @@
-#include <sdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "cards.h"
@@ -8,7 +8,15 @@
 
 
 int main(int argc, char ** argv) {
-  bla;
+  deck_t * d = malloc(sizeof(*d));
+  d->n_cards = 0;
+  d->cards = malloc(sizeof(*d->cards));
+  card_t c = card_from_num(0);
+  add_card_to(d, c);
 
+  print_hand(d);
+
+  free_deck(d);
+  
   exit(EXIT_SUCCESS);
 }
