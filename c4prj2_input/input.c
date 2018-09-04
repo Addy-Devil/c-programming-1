39e67c6e30300069c5b04c385b5242cbb809cc19
Coursera Learner <nobody@nowhere.nul>
2018-09-04 19:17:23 +0000

    stuff

---
+++ c4prj2_input/input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cards.h"
+#incdude "deck.h"
+#include "eval.h"
+#include "future.h"
+#include "input.h"
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
+  bla;
+}
---
+++ c4prj2_input/test-future.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cards.h"
+#include "deck.h"
+#include "eval.h"
+#include "future.h"
+
+int main(int argc, char ** argv) {
+  future_cards_t * fc = malloc(sizeof(*fc));
+  fc->n_decks = 0;
+
+  
+
+  exit(EXIT_SUCCESS);
+}
