70fc27af31a4e665c49eb3e5c4eef09a96cee9e6
Coursera Learner <nobody@nowhere.nul>
2018-09-04 22:34:56 +0000

    s

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -13,10 +13,10 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
   deck->n_cards = 0;
   
   //sep the string on spaces
-  while(str != '\n') {
+  while(*str != '\n') {
     deck->n_cards++;
     deck->cards = realloc(deck->cards, deck->n_cards * sizeof(*deck->cards));
-    if (strcmp(*str, '?') == 0) {
+    if (strcmp(str, '?') == 0) {
       size_t index;
       int n = atoi(*(str+1));
       index = (size_t)n;
