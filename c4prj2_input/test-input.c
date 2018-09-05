e0352d1a3f1d4e37e5c30ba3cc4f7ae96e7e49c1
Coursera Learner <nobody@nowhere.nul>
2018-09-05 00:49:57 +0000

    s

--- c4prj2_input/test-input.c
+++ c4prj2_input/test-input.c
@@ -21,14 +21,26 @@ int main(int argc, char ** argv) {
 
   size_t n_hands = 0;
   future_cards_t * fc = malloc(sizeof(*fc));
+  fc->decks = malloc(sizeof(*fc->decks));
+  fc->n_decks = 0;
 
   deck_t ** deck_ts = read_input(f, &n_hands, fc);
 
   printf("n_hands: %zu\n", n_hands);
   
-  for (int i=0; i<n_cards; i++) {
+  for (int i=0; i<n_hands; i++) {
     print_hand(deck_ts[i]);
   }
+
+  for (int i=0; i<n_hands; i++) {
+    free_deck(deck_ts[i]);
+  }
+
+  for(int i=0; i<fc->n_decks; i++) {
+    free_deck(fc->decks[i]);
+  }
+
+  free(fc);
   
   exit(EXIT_SUCCESS);
 }
