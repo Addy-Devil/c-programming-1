dcf3929a52ab5f4ada17914aecae680e6de02dad
Coursera Learner <nobody@nowhere.nul>
2018-09-20 21:23:36 +0000

    s

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -20,9 +20,9 @@ void malloc_new_decks(future_cards_t * fc, size_t index) {
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
 
   if (fc->n_decks == 0) {
-    fc.decks->cards = NULL;
-    fc.decks->n_cards = 0;
-    fc.n_decks = 1;
+    fc->decks->cards = NULL;
+    fc->decks->n_cards = 0;
+    fc->n_decks = 1;
   }
   
   if (fc->n_decks <= index) {
