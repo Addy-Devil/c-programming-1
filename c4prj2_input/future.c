f3747e810bbc580aae3a3fc6013396724da12167
Coursera Learner <nobody@nowhere.nul>
2018-10-02 18:32:50 +0000

    s

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -52,8 +52,8 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
       for (size_t j=0; j<fc->decks[i].n_cards; j++) {
 	*fc->decks[i].cards[j] = *c;
       }
+      free(c);
     }
-    free(c);
     k++;
   }
 }
