b6bb6ecc6c6b16bd528b5d5104f01ce64ea08a94
Coursera Learner <nobody@nowhere.nul>
2018-10-02 02:35:19 +0000

    s

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -20,6 +20,9 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
 }
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
+  if (fc == NULL) {
+    return;
+  }
   if (deck->n_cards == 0) {
     //fprintf(stderr, "future_cards_from_deck: shuffled_deck->n_cards == 0\n");
     return;
@@ -45,8 +48,7 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
   for (size_t i=0; i<fc->n_decks; i++) {
     if (fc->decks[i].n_cards > 0) {
       for (size_t j=0; j<fc->decks[i].n_cards; j++) {
-	fc->decks[i].cards[j]->suit = deck->cards[k]->suit;
-	fc->decks[i].cards[j]->value = deck->cards[k]->value;
+	*fc->decks[i].cards[j] = *deck->cards[k];
 	k++;
       }
     }
