07c313aa2bc567cce728e6912a1acc24df033053
Coursera Learner <nobody@nowhere.nul>
2018-10-02 18:33:39 +0000

    s

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -48,7 +48,7 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
   for (size_t i=0; i<fc->n_decks; i++) {
     if (fc->decks[i].n_cards > 0) {
       card_t * c = malloc(sizeof(*c));
-      c = deck->cards[k];
+      *c = *deck->cards[k];
       for (size_t j=0; j<fc->decks[i].n_cards; j++) {
 	*fc->decks[i].cards[j] = *c;
       }
